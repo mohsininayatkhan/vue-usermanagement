d0cb34a9e8388313ca8ff65dc254923b2a700105
Mohsin Inayat Khan <mohsin.inayat@gmail.com>
2021-12-29 16:02:33 +0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBuwzTcHeSwpwkDNaC6-LCoLmxjed3XOJE",
-    authDomain: "usermanagement-eb136.firebaseapp.com",
-    projectId: "usermanagement-eb136",
-    storageBucket: "usermanagement-eb136.appspot.com",
-    messagingSenderId: "51008987125",
-    appId: "1:51008987125:web:8850d24647b5fc09574abc"
+    apiKey: "AIzaSyBuwzTcHeSwpwkDNaC6-LCoLmxjed3XOJEA",
+    authDomain: "usermanagement-eb136a.firebaseapp.com",
+    projectId: "usermanagement-eb136a",
+    storageBucket: "usermanagement-eb136a.appspot.com",
+    messagingSenderId: "510089871251",
+    appId: "1:51008987125:web:8850d24647b5fc09574abca"
   }
 
   firebase.initializeApp(firebaseConfig)
